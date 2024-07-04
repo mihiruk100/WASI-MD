b8bfe97ac80e1c2da9323c34a3ab4d60ba2e8eef
mihiruk100 <155881114+mihiruk100@users.noreply.github.com>
2024-07-04 19:42:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ import fs from 'fs'
 
 //Owner Numbers 
 global.owner = [
-  ['923135673658', 'wasi', false],
+  ['94778764083', 'wasi', false],
   [''], 
   [''],
 ]
 
-//global.pairingNumber = "" //put your bot number here
-global.mods = ['923192173398'] 
-global.prems = ['923192173398', '923192173398', '923192173398']
-global.allowed = ['923192173398']
+//global.pairingNumber = "94701073037" //put your bot number here
+global.mods = ['94778764083'] 
+global.prems = ['94778764083', '94766969406', '923192173398']
+global.allowed = ['94778764083']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -40,7 +40,7 @@ global.APIKeys = { // APIKey Here
 global.botname = '𝗪𝗔𝗦𝗜-𝗠𝗗'
 global.princebot = '🛡️𝗪𝗔𝗦𝗜 𝗧𝗘𝗖𝗛🛡️'
 global.packname = '𝗪𝗔𝗦𝗜♥️' 
-global.author = '𝗧𝗘𝗖𝗛♥️' 
+global.author = 'MR MIHIRUK' 
 global.princeig = 'https://www.instagram.com' 
 global.princegp = 'https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j'
 global.menuvid = 'https://i.imgur.com/0UK6D3b.mp4'
